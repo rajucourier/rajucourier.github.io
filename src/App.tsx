640906d27f81e4938c980f9df86541e788695fc2
manoj datla <mpvarmadatla@outlook.com>
2025-05-01 18:16:11 +0530

    add tailwindcss and react-router package

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        
-      <h1>Raju Courier will be available soon.</h1>
-      </div>
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
