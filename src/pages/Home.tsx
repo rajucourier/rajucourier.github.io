523b1ab3d1997527202267feb1b6fe69dfff08e6
manoj datla <mpvarmadatla@outlook.com>
2025-05-03 11:13:44 +0530

    move the image from assests to public folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,9 @@
 import { JSX } from "react";
+import Highlight from "../components/Highlight";
 
 const Home = (): JSX.Element => {
     return <div className="bg-[#F2F2F2]">
-        <section className="w-full px-4 py-12 h-auto bg-[#1B284D]">
-            <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center">
-
-                {/* <!-- Text Section --> */}
-                <div className="w-full md:w-1/2 mb-8 md:mb-0 ">
-                    <h2 className="text-3xl font-bold mb-4 text-white">SHOP FROM INDIA &</h2>
-                    <h2 className="text-3xl font-bold mb-4 text-white">SHIP WORLDWIDE</h2>
-                   
-                        <div className="bg-[#F7AA18] shadow-lg rounded-lg p-6">
-                           <ul className="list-disc list-inside font-semibold text-xl">
-                                <li className="mb-2">Shop from 1000+ Indian websites & Instagram Stories</li>
-                                <li className="mb-2">Ship to 6+ countries</li>
-                                <li className="mb-2">Custom Support</li>
-                            </ul>
-                        </div>
-                    
-                </div>
-
-                {/* <!-- Image Section --> */}
-                <div className="w-full md:w-1/2">
-                    <img
-                        src="src\assets\box.svg"
-                        alt="Placeholder Image"
-                        className="w-full h-60 rounded shadow"
-                    />
-                </div>
-
-            </div>
-        </section>
+       <Highlight/>
     </div>
 };
 
